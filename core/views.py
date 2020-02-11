d259ca12645ab5b859267bd6d940749b73a6e5b1
LuSoMaBra <lusomabra@gmail.com>
2020-02-11 15:48:09 +0000

    Tests de github from PyCharm

--- core/views.py
+++ core/views.py
@@ -3,6 +3,9 @@ from core.models import Evento
 
 # Create your views here.
 
+# def index(request):
+#     return redirect('/agenda/')
+
 def lista_eventos(request):
     evento = Evento.objects.all()
     dados = {'eventos':evento}
