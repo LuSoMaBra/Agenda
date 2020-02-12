74b5267975e80e281b8f5687c66c4fb3e063ff02
LuSoMaBra <lusomabra@gmail.com>
2020-02-12 10:37:29 +0000

    Finaly Teste GitHub

--- core/views.py
+++ core/views.py
@@ -47,5 +47,13 @@ def submit_evento(request):
         descricao = request.POST.get('descricao')
         usuario = request.user
         Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
-
     return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
+    return redirect('/')
+
